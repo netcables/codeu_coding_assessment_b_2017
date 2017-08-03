134c6bebaf920cfbfdb5ce66e530eccbd8a8b0ae
Antonio Carmona <adcarmona98@gmail.com>
2017-08-03 15:45:40 -0700

    Rough solution for most tests
    Added a rough solution that passes all of the existing tests except for the final one.

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -15,8 +15,13 @@
 package com.google.codeu.mathlang.impl;
 
 import java.io.IOException;
+import java.lang.StringBuilder;
 
 import com.google.codeu.mathlang.core.tokens.Token;
+import com.google.codeu.mathlang.core.tokens.StringToken;
+import com.google.codeu.mathlang.core.tokens.NameToken;
+import com.google.codeu.mathlang.core.tokens.SymbolToken;
+import com.google.codeu.mathlang.core.tokens.NumberToken;
 import com.google.codeu.mathlang.parsing.TokenReader;
 
 // MY TOKEN READER
@@ -26,10 +31,14 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // You should not need to change any other files to get your token reader to
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
+  private StringBuilder currentToken = new StringBuilder();
+  private int currentIndex = 0;
+  private String source;
 
   public MyTokenReader(String source) {
     // Your token reader will only be given a string for input. The string will
     // contain the whole source (0 or more lines).
+    this.source = source;
   }
 
   @Override
@@ -40,7 +49,96 @@ public final class MyTokenReader implements TokenReader {
 
     // If for any reason you detect an error in the input, you may throw an IOException
     // which will stop all execution.
-
-    return null;
+    while(remaining() > 0 && Character.isWhitespace(peek())) {
+      read();
+    }
+    if (remaining() <= 0) {
+      System.out.println("Returning null!");
+      return null;
+    } else if (peek() == '"') {
+      return interpretToken(readWithQuotes());
+    } else {
+      return interpretToken(readWithNoQuotes());
+    }
   }
+  
+  // Helper function to check remaining characters in source string.
+  private int remaining() {
+    return source.length() - currentIndex;
+  }
+  
+  // Helper function to check the character at the current index of the source string.
+  private char peek() throws IOException {
+    if(currentIndex < source.length()) {
+      return source.charAt(currentIndex);
+    } else {
+      throw new IOException("Peek error.");
+    }
+  }
+
+  // Helper function to read the character at the current index of the source string.
+  private char read() throws IOException {
+    final char c = peek();
+    currentIndex += 1;
+    return c;
+  }
+  
+  // Helper function to read text within quotes (notes).
+  private String readWithQuotes() throws IOException {
+    currentToken.setLength(0);
+    if (read() != '"') {
+      throw new IOException("Strings must start with quote.");
+    }
+    while (peek() != '"') {
+      currentToken.append(read());
+    }
+    read();
+    System.out.println(currentToken.toString());
+    return currentToken.toString();
+  }
+  
+  // Helper function to read text outside of quotes.
+  private String readWithNoQuotes() throws IOException {
+    currentToken.setLength(0);
+    while(remaining() > 0 && !Character.isWhitespace(peek())) {
+    	if(peek() == ';' && currentToken.length() > 0) {
+    		return currentToken.toString();
+    	} else {
+    		currentToken.append(read());
+    	}
+    }
+    System.out.println(currentToken.toString());
+    return currentToken.toString();
+  }
+  
+  // Helper function to interpret different types of tokens
+  private Token interpretToken(String tokenContent) {
+	  if(tokenContent.length() == 1) {
+		  switch(tokenContent.charAt(0)) {
+		  	case '+':
+		  	case '-':
+		  	case '=':
+		  	case ';':
+		  		SymbolToken returnSymbolToken = new SymbolToken(tokenContent.charAt(0));
+				return returnSymbolToken;
+		  	case 'x':
+		  	case 'y':
+		  	case 'z':
+				NameToken returnNameToken = new NameToken(tokenContent);
+				return returnNameToken;
+		  	default:
+		  		NumberToken returnNumberToken = new NumberToken(Double.parseDouble(tokenContent));
+		  		return returnNumberToken;
+		  }
+		// Name Tokens
+	  } else if(tokenContent.equals("note") || tokenContent.equals("print") || tokenContent.equals("let")) {
+		  NameToken returnNameToken = new NameToken(tokenContent);
+		  return returnNameToken;
+		// String Tokens
+	  } else {
+		  StringToken returnStringToken = new StringToken(tokenContent);
+		  return returnStringToken;
+	  }
+  }
+
 }
